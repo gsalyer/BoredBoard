f5c48ffbc4082e0c1fa1c8166393dba9c4e2c6b6
gsalyer <garrettasalyer@gmail.com>
2022-10-31 17:58:20 -0700

    start js

---
+++ application/public/js/validation.js
@@ -0,0 +1,14 @@
+/*
+- require the user to enter a username that begins with a character ([a-zA-Z]).
+- require the user to enter a username that is 3 or more alphanumeric characters.
+- require the user to enter a password that is 8 or more characters AND contains at least
+    1 upper case letter AND 1 number and 1 of the following special characters:
+    / * - + ! @ # $ ^ & ~ [ ]
+- require that the password and confirm password inputs are the same.
+- require the user to enter an email that is valid.
+    ▪ This one CAN BE done with the type attribute set to “email”
+- require the user to select that they are 13+ years of age.
+    ▪ This one CAN BE done with the HTML attribute “required”
+- require the user to select TOS and Privacy rules.
+    ▪ This one CAN BE done with the HTML attribute “required”
+*/

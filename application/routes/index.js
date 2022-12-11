14bb846074fab6751eb552820e2847dfb2118f07
gsalyer <garrettasalyer@gmail.com>
2022-12-10 19:39:37 -0800

    started handlebars

--- application/routes/index.js
+++ application/routes/index.js
@@ -2,8 +2,24 @@ var express = require('express');
 var router = express.Router();
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'CSC 317 App', name:"Garrett Salyer" });
+router.get('/', function (req, res, next) {
+  res.render('index', { title: 'CSC 317 App', name: "Garrett Salyer" });
+});
+
+router.get("/login", function (req, res) {
+  res.render('login');
+});
+
+router.get("/register", function (req, res) {
+  res.render('registration');
+});
+
+router.get("/postimage", function (req, res) {
+  res.render('postimage');
+});
+
+router.get("/posts/:id(\\d+)", function (req, res) {
+  res.render('viewpost');
 });
 
 module.exports = router;

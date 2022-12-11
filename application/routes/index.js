63da7604eacff5cf73fa97d22b8d2ca998c65af7
gsalyer <garrettasalyer@gmail.com>
2022-12-11 03:36:19 -0800

    changed the pages a bit

--- application/routes/index.js
+++ application/routes/index.js
@@ -3,7 +3,11 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function (req, res, next) {
-  res.render('index', { js: ["fetch.js"], title: 'CSC 317 App', name: "Garrett Salyer" });
+  res.render('home', { title: 'CSC 317 App', name: "Garrett Salyer" })
+})
+
+router.get('/index', function (req, res) {
+  res.render('index', { js: ["fetch.js"] });
 });
 
 router.get("/login", function (req, res) {
